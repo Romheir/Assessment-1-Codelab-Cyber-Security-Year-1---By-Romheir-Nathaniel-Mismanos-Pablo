75323fe1b13476836d8155e9a015467b60eb5563
Romheir <romheir.nmp09@gmail.com>
2025-11-25 00:23:34 +0400

    Update Exercise9.py

--- fall-2025-code-lab-1-assignement-1-programming-skills-portfolio-Programming-Skills-Portfolio-Fall25/Exercise 09 -Hello/Exercise9.py
+++ fall-2025-code-lab-1-assignement-1-programming-skills-portfolio-Programming-Skills-Portfolio-Fall25/Exercise 09 -Hello/Exercise9.py
@@ -4,3 +4,9 @@ def main():
     hello() #this calls the function "Hello"
 if __name__ == "__main__":
     main()
+#the output of this code would be "Hello"
+
+#but for a more simplified version, simply doing: print ("Hello")
+
+#or doing variable = Hello and doing: print (variable) also works to minimize and make the code more efficient.
+
