511ed676b4299701c5a5bcd405d9bef633eeeb68
Romheir <romheir.nmp09@gmail.com>
2025-11-25 00:14:42 +0400

    Update Exercise6.py

--- fall-2025-code-lab-1-assignement-1-programming-skills-portfolio-Programming-Skills-Portfolio-Fall25/Exercise 06 -Brute Force Attack/Exercise6.py
+++ fall-2025-code-lab-1-assignement-1-programming-skills-portfolio-Programming-Skills-Portfolio-Fall25/Exercise 06 -Brute Force Attack/Exercise6.py
@@ -1,7 +1,7 @@
 correct_password = "12345"
-attemps_left = 5
+attempts_left = 5
 
-while attemps_left > 0:
+while attempts_left > 0:
     password = input ("Please enter your password: ")
 
     if password == correct_password:
@@ -14,3 +14,4 @@ while attemps_left > 0:
         print(f"This is the wrong password. You have {attempts_left} attempts left.")
     else:
         print("You have reached the maximum amount of attempts, the authorities have been alerted.")
+
