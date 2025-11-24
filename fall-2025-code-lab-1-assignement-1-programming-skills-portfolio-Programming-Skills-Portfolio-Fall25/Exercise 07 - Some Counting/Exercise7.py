d7ef4f9441d04018e7332e0a70cb9a7cd30c897a
Romheir <romheir.nmp09@gmail.com>
2025-11-25 00:19:23 +0400

    Update Exercise7.py

--- fall-2025-code-lab-1-assignement-1-programming-skills-portfolio-Programming-Skills-Portfolio-Fall25/Exercise 07 - Some Counting/Exercise7.py
+++ fall-2025-code-lab-1-assignement-1-programming-skills-portfolio-Programming-Skills-Portfolio-Fall25/Exercise 07 - Some Counting/Exercise7.py
@@ -1,10 +1,14 @@
-for i in range(0, 51, 1):
-    print(i)
-for i in range (50, -1, -1):
-    print(i)
-for i in range(30, 51, 1):
-    print(i)
-for i in range (50, 9, -2):
-    print(i)
-for i in range(100, 201, 5):
-    print(i)
+for i in range(0, 51, 1): #this line of coed starts a loop that starts counting from 0. It stops when you reach 51, and adds 1 everytime. (It does not include 51)
+    print(i) #this is the output
+for i in range (50, -1, -1): #this line starts another loop that starts at 50, it stps when you reach -1, but does not incldue -1. It subtracts 1 each time.
+    print(i) #execution, the output
+for i in range(30, 51, 1): #this is yet another loop that starts at 30, and ends before 51. This counts up by 1
+    print(i), #execution
+    print(i) #execution
+for i in range (50, 9, -2): #this starts another loop that starts at 50 and stops before 10, it counts down by 2.
+    print(i) #execution
+for i in range(100, 201, 5): #this final loop starts at 100, and stops before 201. It adds 5 everytime
+
+    print(i) #execution
+
+    #we then get all the variables together.
